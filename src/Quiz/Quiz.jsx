3a6c4b1d72b2d022c9cf74d5d7fbd09a9c032f71
harsh0506 <joshiharsh0506@gmail.com>
2023-05-22 18:10:46 +0530

    added quiz compatibiltiy

--- src/Quiz/Quiz.jsx
+++ src/Quiz/Quiz.jsx
@@ -2,69 +2,38 @@ import { useState, useEffect } from "react";
 import Navbar from "../LandingPage/Navbar";
 import { openDB } from 'idb';
 import { useLocation } from "react-router-dom";
-const questions = [
-  {
-    id: 1,
-    question: "What is the capital of France?",
-    option1: "London",
-    option2: "Paris",
-    option3: "Rome",
-    option4: "Berlin",
-    ans: "Paris",
-  },
-  {
-    id: 2,
-    question: "What is the largest planet in our solar system?",
-    option1: "Mars",
-    option2: "Jupiter",
-    option3: "Venus",
-    option4: "Saturn",
-    ans: "Jupiter",
-  },
-  // Add more questions here
-];
+
 function App() {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [userAnswers, setUserAnswers] = useState({});
   const [showResult, setShowResult] = useState(false);
-  const [isAnswerSelected, setIsAnswerSelected] = useState(false);
   const [testData, setTestData] = useState(null);
-  const [IdOfTest, setIdOfTest] = useState(0)
-  const [recent, setRecent] = useState([])
+  const [selectedTestId, setSelectedTestId] = useState(0);
+  const [testStarted, setTestStarted] = useState(false);
+  const [isAnswerSelected, setIsAnswerSelected] = useState(false);
+
   const { state } = useLocation();
   const { name } = state;
-  const subject = name || "Science: Mathematics".trim()
-
-
+  const subject = name || "Science: Mathematics";
 
   useEffect(() => {
     const fetchData = async () => {
-      console.log()
       try {
-        // Open the IndexedDB database
         const db = await openDB('testDB', 1, {
           upgrade(db) {
-            // Create an object store for the cache
             db.createObjectStore('cache');
           },
         });
 
-        // Check if the test data is available in the cache
         const cachedData = await db.get('cache', subject);
 
         if (cachedData) {
-          console.log("data from cache")
-          // If data is available in the cache, use it
           setTestData(cachedData);
         } else {
-          // If data is not available in the cache, fetch it from the API
           const response = await fetch(import.meta.env.VITE_SPECIFIC_DATA + subject);
           const fetchedData = await response.json();
 
-          // Store the fetched data in the cache
           await db.put('cache', fetchedData, subject);
-          console.log("data from api")
-
           setTestData(fetchedData);
         }
       } catch (error) {
@@ -75,9 +44,32 @@ function App() {
     fetchData();
   }, [subject]);
 
- 
+  const handleStartTest = () => {
 
-  const handleOptionSelect = (questionId, selectedOption) => {
+    if (selectedTestId === 0) {
+      alert("Please select a test.");
+      return;
+    }
+
+    if (testData && testData.length > 0) {
+      setShowResult(false);
+      setCurrentQuestionIndex(0);
+      setUserAnswers({});
+      setTestStarted(true);
+    } else {
+      window.alert("Test data is not available. Please try again later.");
+    }
+  };
+
+  const handleResetQuiz = () => {
+    setCurrentQuestionIndex(0);
+    setUserAnswers({});
+    setShowResult(false);
+    setTestStarted(false);
+  };
+
+  const handleOptionSelect = (event, questionId) => {
+    const selectedOption = event.target.value;
     setUserAnswers((prevUserAnswers) => ({
       ...prevUserAnswers,
       [questionId]: selectedOption,
@@ -87,167 +79,184 @@ function App() {
 
   const handleNextQuestion = () => {
     if (isAnswerSelected) {
-      if (currentQuestionIndex + 1 < questions.length) {
+      if (currentQuestionIndex + 1 < testData[selectedTestId].length) {
         setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
       } else {
         setShowResult(true);
       }
       setIsAnswerSelected(false);
     } else {
-      window.alert("Please select an answer before submitting.");
+      alert("Please select an answer before submitting.");
     }
-  };
-
-  const handleResetQuiz = () => {
-    setCurrentQuestionIndex(0);
-    setUserAnswers({});
-    setShowResult(false);
   };
 
   const calculateScore = () => {
     let score = 0;
-    questions.forEach((question) => {
+    testData[selectedTestId].forEach((question) => {
       const userAnswer = userAnswers[question.id];
-      if (userAnswer && userAnswer === question.ans) {
+      console.log(userAnswer)
+      if (userAnswer && userAnswer === question.answer) {
         score += 1;
       }
     });
     return score;
   };
 
-  const renderQuestion = (question) => {
+  const renderQuestion = () => {
+    if (!testData || testData.length === 0) {
+      return null;
+    }
+
+    const question = testData[selectedTestId][currentQuestionIndex];
     const { id, question: text, option1, option2, option3, option4 } = question;
 
     return (
 
-      <div key={id} style={{
-        width: "100vw",
-        height: "100vh"
-      }} className="flex items-center justify-center h-screen bg-gray-100">
-        <div className="max-w-3xl w-full mx-4 md:mx-auto p-6 bg-white rounded-lg shadow-md">
-          <div style={{ display: "flex", gap: 30 }}>
-            <h2 className="text-3xl font-bold mb-6">Quiz</h2>
-
-            <div className="sm:col-span-3">
-              <div className="mt-2">
-                <select
-                  onChange={(e) => setIdOfTest(parseInt(e.target.value))}
-                  id="country"
-                  name="country"
-                  autoComplete="country-name"
-                  className="block w-full rounded-md border-0 py-1.5 text-black bg-white shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
-                >
-                  {testData && testData.length > 0 ? (
-                    testData.map((todo, index) => (
-                      <option value={index} key={index}>{index}</option>
-                    ))
-                  ) : (
-                    <p>0</p>
-                  )}
-
-                </select>
-              </div>
-            </div>
-
-          </div>
-          <form onSubmit={null}>
-            <div className="mb-8">
-              <p className="font-medium text-xl">Question:</p>
-              <p className="mt-2 text-lg">What is the capital of France?</p>
-            </div>
-
-            <div className="mb-8">
-              <label className="block mb-2 text-lg font-semibold">
-
-                <input
-                  type="radio"
-                  value={option1}
-                  checked={userAnswers[id] === option1}
-                  onChange={() => handleOptionSelect(id, option1)}
-                />
-                <span className="ml-2">{option1}</span>
-              </label>
-
-              <label className="block mb-2 text-lg font-semibold">
-                <input
-                  type="radio"
-                  value={option2}
-                  checked={userAnswers[id] === option2}
-                  onChange={() => handleOptionSelect(id, option2)}
-                />
-                <span className="ml-2">{option2}</span>
-              </label>
-
-              <label className="block mb-2 text-lg font-semibold">
-                <input
-                  type="radio"
-                  value={option3}
-                  checked={userAnswers[id] === option3}
-                  onChange={() => handleOptionSelect(id, option3)}
-                />
-                <span className="ml-2">{option3}</span>
-              </label>
-
-              <label className="block mb-2 text-lg font-semibold">
-                <input
-                  type="radio"
-                  value={option4}
-                  checked={userAnswers[id] === option4}
-                  onChange={() => handleOptionSelect(id, option4)}
-                />
-                <span className="ml-2">{option4}</span>
-              </label>
-            </div>
-
-            <button
-              onClick={handleNextQuestion}
-              type="submit"
-              className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            >
-              Submit
-            </button>
-          </form>
+      <div style={{ width: "100vw" }} className="max-w-3xl mx-auto p-6 bg-white rounded-lg shadow-md">
+        <h2 className="text-3xl font-bold mb-6">Quiz</h2>
+        <div className="mb-8">
+          <p className="font-medium text-xl">Question:</p>
+          <p className="mt-2 text-lg">{text}</p>
         </div>
+        <div className="mb-8">
+          <label className="block mb-2 text-lg font-semibold">
+            <input
+              type="radio"
+              value={option1}
+              checked={userAnswers[id] === option1}
+              onChange={(e) => handleOptionSelect(e, id)}
+              disabled={!testStarted}
+            />
+            <span className="ml-2">{option1}</span>
+          </label>
+          <label className="block mb-2 text-lg font-semibold">
+            <input
+              type="radio"
+              value={option2}
+              checked={userAnswers[id] === option2}
+              onChange={(e) => handleOptionSelect(e, id)}
+              disabled={!testStarted}
+            />
+            <span className="ml-2">{option2}</span>
+          </label>
+          <label className="block mb-2 text-lg font-semibold">
+            <input
+              type="radio"
+              value={option3}
+              checked={userAnswers[id] === option3}
+              onChange={(e) => handleOptionSelect(e, id)}
+              disabled={!testStarted}
+            />
+            <span className="ml-2">{option3}</span>
+          </label>
+          <label className="block mb-2 text-lg font-semibold">
+            <input
+              type="radio"
+              value={option4}
+              checked={userAnswers[id] === option4}
+              onChange={(e) => handleOptionSelect(e, id)}
+              disabled={!testStarted}
+            />
+            <span className="ml-2">{option4}</span>
+          </label>
+        </div>
+        <button
+          onClick={handleNextQuestion}
+          className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          disabled={!testStarted}
+        >
+          Next
+        </button>
       </div>
+
     );
   };
 
   const renderResult = () => {
+    if (!testData || testData.length === 0) {
+      return null;
+    }
+
     const score = calculateScore();
+    const totalQuestions = testData[selectedTestId].length;
+    const remainingQuestions = totalQuestions - currentQuestionIndex - 1;
+
     return (
+      <div className="max-w-3xl mx-auto p-6 bg-white rounded-lg shadow-md">
+        <h2 className="text-3xl font-bold mb-6">Quiz Result</h2>
+        <p className="text-lg">
+          Your score: {score}/{totalQuestions}
+        </p>
+        <p className="text-lg mt-2">
+          Questions done: {currentQuestionIndex + 1}/{totalQuestions}
+        </p>
+        <p className="text-lg mt-2">
+          Questions remaining: {remainingQuestions}
+        </p>
+        <div className="mt-6">
+          <button
+            onClick={handleResetQuiz}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            disabled={testStarted}
+          >
+            Reset Test
+          </button>
 
-      <div style={{
-        width: "100vw",
-        height: "100vh"
-      }} className="flex items-center justify-center h-screen bg-white-100">
-        <div className="max-w-3xl w-full mx-4 md:mx-auto p-6 bg-white rounded-lg shadow-md">
-          <h2 className="text-4xl font-bold mb-6">Quiz Result</h2>
-
-          <div className="mb-8">
-            <p className="text-2xl font-semibold">Congratulations!</p>
-            <p className="text-xl mt-2">Your score: {score}/{questions.length}</p>
-            <button onClick={handleResetQuiz}>Reset Quiz</button>
-          </div>
-
-          <p className="text-lg">Thank you for taking the quiz.</p>
         </div>
-      </div>
+      </div >
     );
   };
 
   return (
-    <div>
+    <div style={{ width: "100vw" }}>
       <Navbar />
-      {showResult ? (
-        renderResult()
-      ) : (
-        <div>
-          {String(testData[IdOfTest][currentQuestionIndex].id)}
-          {renderQuestion(questions[currentQuestionIndex])}
-        </div>
-      )}
+      <div className="flex items-center justify-center h-screen bg-gray-100">
+        {testData ? (
+          <div className="flex flex-col items-center">
+            {!testStarted ? (
+              <div className="max-w-3xl mx-auto p-6 bg-white rounded-lg shadow-md">
+                <h2 className="text-3xl font-bold mb-6">Select Test</h2>
+                <div className="mb-8">
+                  <select
+                    className="w-full rounded-md border-0 py-1.5 px-3 text-lg"
+                    value={selectedTestId}
+                    onChange={(e) => setSelectedTestId(Number(e.target.value))}
+                  >
+                    {Object.keys(testData).map((testId) => (
+                      <option key={testId} value={testId}>
+                        Test {testId}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+                <button
+                  onClick={handleStartTest}
+                  className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                >
+                  Start Test
+                </button>
+              </div>
+            ) : (
+              <div>
+                <div className="flex justify-between items-center mb-4">
+                  <p className="text-lg font-medium">
+                    Question {currentQuestionIndex + 1} of {testData[selectedTestId].length}
+                  </p>
+                  <p className="text-lg font-medium">
+                    {testData[selectedTestId].length - currentQuestionIndex - 1} questions remaining
+                  </p>
+                </div>
+                {showResult ? renderResult() : renderQuestion()}
+              </div>
+            )}
+          </div>
+        ) : (
+          <p>Loading test data...</p>
+        )}
+      </div>
     </div>
+
   );
 }
 
-export default App
+export default App;
