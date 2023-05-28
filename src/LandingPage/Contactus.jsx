58465a6a5ebe83a2874ee1b1fcdff2d646346f10
Harsh  Joshi <78259306+harsh0506@users.noreply.github.com>
2023-05-28 14:29:38 +0530

    Update Contactus.jsx

--- src/LandingPage/Contactus.jsx
+++ src/LandingPage/Contactus.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "./Contactus.css"
+import "./ContactUs.css"
 const ContactUsPage = () => {
 
     const [name, setNAme] = useState()
